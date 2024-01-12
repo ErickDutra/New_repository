fd0348082550639dd00a6cf4b768f49545426d4e
Erick Dutra <107477302+ErickDutra@users.noreply.github.com>
2024-01-12 02:26:50 -0300

    created pop-up info

---
+++ assets/js/pop-dialog.js
@@ -0,0 +1,60 @@
+
+const button1 = document.querySelector(".open-popup-1")
+const modal_project1 = document.querySelector(".project-1")
+const buttonClose1 = document.querySelector(".project-1 button")
+
+button1.onclick = function(){
+    modal_project1.showModal()
+}
+
+buttonClose1.onclick = function(){
+    modal_project1.close()
+}
+
+const button2 = document.querySelector(".open-popup-2")
+const modal_project2 = document.querySelector(".project-2")
+const buttonClose2 = document.querySelector(".project-2 button")
+
+button2.onclick = function(){
+    modal_project2.showModal()
+}
+
+buttonClose2.onclick = function(){
+    modal_project2.close()
+}
+
+const button3 = document.querySelector(".open-popup-3")
+const modal_project3 = document.querySelector(".project-3")
+const buttonClose3 = document.querySelector(".project-3 button")
+
+button3.onclick = function(){
+    modal_project3.showModal()
+}
+
+buttonClose3.onclick = function(){
+    modal_project3.close()
+}
+
+const button4 = document.querySelector(".open-popup-4")
+const modal_project4 = document.querySelector(".project-4")
+const buttonClose4 = document.querySelector(".project-4 button")
+
+button4.onclick = function(){
+    modal_project4.showModal()
+}
+
+buttonClose4.onclick = function(){
+    modal_project4.close()
+}
+
+const button5 = document.querySelector(".open-popup-5")
+const modal_project5 = document.querySelector(".project-5")
+const buttonClose5 = document.querySelector(".project-5 button")
+
+button5.onclick = function(){
+    modal_project5.showModal()
+}
+
+buttonClose5.onclick = function(){
+    modal_project5.close()
+}
