2cba4936903fb83aa54f7fe76b1f799e92b200d9
Erick Dutra <107477302+ErickDutra@users.noreply.github.com>
2024-06-28 01:47:22 -0300

    update template

--- assets/js/carrosel.js
+++ assets/js/carrosel.js
@@ -34,3 +34,19 @@ controls.forEach((control) => {
     items[currentItem].classList.add("current-item");
   });
 });
+
+document.addEventListener("DOMContentLoaded", function() {
+  const items = document.querySelectorAll('.skills .skills-itens');
+
+  items.forEach((item) => {
+    item.addEventListener('animationend', function() {
+   
+      const parent = item.parentNode;
+      parent.appendChild(item);
+    
+      item.classList.remove('skills-itens');
+      void item.offsetWidth;
+      item.classList.add('skills-itens');
+    });
+  });
+});
