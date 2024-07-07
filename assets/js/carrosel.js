1350028f20b90c92da229eaecf87e260250279c0
Erick Dutra <107477302+ErickDutra@users.noreply.github.com>
2024-07-06 23:23:48 -0300

    projetos

--- assets/js/carrosel.js
+++ assets/js/carrosel.js
@@ -1,39 +1,3 @@
-const controls = document.querySelectorAll(".control");
-let currentItem = 0;
-const items = document.querySelectorAll(".item");
-const maxItems = items.length;
-
-controls.forEach((control) => {
-  control.addEventListener('click', (e) => {
-    isRight = e.target.classList.contains("arrow-right");
-
-
-    if (isRight) {
-      currentItem += 1;
-    }
-    else {
-      currentItem -= 1;
-    };
-
-
-    if (currentItem >= maxItems) {
-      currentItem = 0;
-    }
-
-    if (currentItem < 0) {
-      currentItem = maxItems - 1;
-    }
-
-    items.forEach((item) => item.classList.remove("current-item"));
-
-    items[currentItem].scrollIntoView({
-      behavior: "instant",
-      inline: "center"
-    });
-
-    items[currentItem].classList.add("current-item");
-  });
-});
 
 document.addEventListener("DOMContentLoaded", function() {
   const items = document.querySelectorAll('.skills .skills-itens');
