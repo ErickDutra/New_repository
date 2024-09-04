f52508864fe8f6fec18a09bf5f884c194d577d94
Erick Dutra <107477302+ErickDutra@users.noreply.github.com>
2024-09-04 00:16:25 -0300

    update project

--- assets/js/carrosel.js
+++ assets/js/carrosel.js
@@ -1,16 +1,21 @@
-
-document.addEventListener("DOMContentLoaded", function() {
-  const items = document.querySelectorAll('.skills .skills-itens');
-
-  items.forEach((item) => {
-    item.addEventListener('animationend', function() {
-   
-      const parent = item.parentNode;
-      parent.appendChild(item);
-    
-      item.classList.remove('skills-itens');
-      void item.offsetWidth;
-      item.classList.add('skills-itens');
-    });
-  });
+document.getElementById('about').addEventListener('click', function() {
+  showSection('about-section');
 });
+
+document.getElementById('home').addEventListener('click', function() {
+  showSection('home-section');
+});
+
+document.getElementById('projects').addEventListener('click', function() {
+  showSection('projects-section');
+});
+
+function showSection(sectionId) {
+  // Esconde todas as seções
+  document.getElementById('about-section').style.display = 'none';
+  document.getElementById('home-section').style.display = 'none';
+  document.getElementById('projects-section').style.display = 'none';
+
+  // Mostra a seção selecionada
+  document.getElementById(sectionId).style.display = 'block';
+}
--- assets/js/dados.js
+++ assets/js/dados.js
@@ -1,44 +1,37 @@
 async function fetchProfileData() {
-    try {
-        const response = await fetch('https://raw.githubusercontent.com/ErickDutra/New_repository/main/assets/data/profile.json');
-        if (!response.ok) {
-            throw new Error('Erro ao buscar os dados do perfil');
-        }
-        return await response.json();
-    } catch (error) {
-        console.error('Erro ao buscar os dados do perfil:', error);
-        return null;
+  try {
+    const response = await fetch(
+      "https://raw.githubusercontent.com/ErickDutra/New_repository/main/assets/data/profile.json"
+    );
+    if (!response.ok) {
+      throw new Error("Erro ao buscar os dados do perfil");
     }
+    return await response.json();
+  } catch (error) {
+    console.error("Erro ao buscar os dados do perfil:", error);
+    return null;
+  }
 }
 
 async function updatePortfolio() {
     try {
-        const profileData = await fetchProfileData();
-        if (profileData) {
-            const portfolio = document.getElementById('profile.portfolio');
-            portfolio.innerHTML = profileData.itens.map(item => {
-                return `<li class="item-carrosel current-item item  " >
-                <h2>
-                   ${item.title} 
-                </h2>
-                <img src="${item.imagem}" alt="${item.title}" class="project-img">
-                    <div class="project-info">
-                    <p>
-                        ${item.descricao}
-                    </p>
-                    <a href="${item.url}">
-                        <img src="./assets/icons/github.svg" alt="github">
-                        <p>
-                            ir para Github
-                        </p>
-                    </a>
-                </div>
-            </li>`;
-            }).join('');
-        }
-    } catch (error) {
-        console.error('Erro ao atualizar o portfólio:', error);
-    }
-}
+  const profileData = await fetchProfileData();
+  if (profileData) {
+    const portfolio = document.getElementById("projects-card");
+    portfolio.innerHTML = profileData.itens
+      .map((item) => {
+        return `<div class="projects-box-item">
+                            <img class="img-stack" src="${item.imagem}" alt="">
+                            <h2>${item.title}</h2>
+                            <p>${item.descricao}</p>
+                            <a href="${item.url}" class="btn-image" ><img src="https://img.shields.io/badge/github-%23121011.svg?style=for-the-badge&logo=github&logoColor=purple" alt=""></a>
+                        </div>`;
+      })
+      .join("");
+  }
+  
+}catch (error) {
+    console.error('Erro ao atualizar o portfólio:', error);
+} }
 
 updatePortfolio();
